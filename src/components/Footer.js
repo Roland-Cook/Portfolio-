4bb8d12fdc50d0e141bb9a561ad91b8b1caf9827
Roland <cook.rolandcook.roland@gmail.com>
2023-11-10 22:56:20 -0500

    Updated footer and cleaned up code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,8 +15,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon" id="footer-icons">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/roland-cook-6868b5268/" target="__blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://gitlab.com/Cook.r"><img src={navIcon2} alt=""/></a>
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
