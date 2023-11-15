38ec007de7e12ae895af7149d05fcced5b2faf99
Roland <cook.rolandcook.roland@gmail.com>
2023-11-15 13:51:33 -0500

    Integrated node mailer for contact form

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-// server used to send send emails
+// server used to send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+
+
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: "cookroland56@gmail.com",
-    pass: "123"
+    pass: "uccf njbz thiu egdj"
   },
 });
 
